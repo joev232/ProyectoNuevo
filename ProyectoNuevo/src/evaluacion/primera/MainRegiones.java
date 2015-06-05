b3c93e69f5c7739e96c01c566fd992069016f462
joev232 <joev232@hotmail.com>
2015-06-05 21:19:52 +0200

    hi

--- ProyectoNuevo/src/evaluacion/primera/MainRegiones.java
+++ ProyectoNuevo/src/evaluacion/primera/MainRegiones.java
@@ -20,13 +20,15 @@ public class MainRegiones {
 			
 			//pruebo recuperar registro
 			List<RegionsDTO> lista_reg=RegionsDAO.obteneConsulta();
-			System.out.println(lista_reg);
+			//System.out.println(lista_reg);
 			
-			
-			List<RegionsDTO> lista_regiones=new ArrayList<RegionsDTO>();
-			RegionsDAO regionsDAO = new RegionsDAO();
-			lista_regiones=regionsDAO.recuperarRegions(2);
-			System.out.println(lista_regiones);
+			System.out.println(RegionsDAO.recuperarRegions(1));
+			//System.out.println(RegionDAO.leerRegion(5));
+			//Prueba de consultar regions por id
+//			List<RegionsDTO> lista_regiones=new ArrayList<RegionsDTO>();
+//			RegionsDAO regionsDAO = new RegionsDAO();
+//			lista_regiones=regionsDAO.recuperarRegions(2);
+//			System.out.println(lista_regiones);
 			
 		}
 
--- ProyectoNuevo/src/evaluacion/primera/RegionsDAO.java
+++ ProyectoNuevo/src/evaluacion/primera/RegionsDAO.java
@@ -11,6 +11,11 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * @author alumno
+ *
+ */
 
 
 public class RegionsDAO {
@@ -61,40 +66,72 @@ public class RegionsDAO {
 		return lista_regions;
 	}
 	
-	public List<RegionsDTO> recuperarRegions(int region_id)		
-	{
-			Connection conexion = null; 
-	 		Statement stmt = null; 
-	 		PreparedStatement pstmt=null;
-	 		ResultSet rs = null; 
-	 		RegionsDTO regionsDTO= null;
-	 		List<RegionsDTO> lista_regions=new ArrayList<RegionsDTO>();
-	 		
-	 		
-	 		
-			try {
-				conexion = Conexion.obtenerConexion();
-//				pstmt = conexion.prepareStatement(stmt);
-				pstmt = conexion.prepareStatement(InstruccionSQL.RECUPERAR_REGIONES);
-				pstmt.setInt(1,region_id);
-				rs=pstmt.executeQuery(InstruccionSQL.RECUPERAR_REGIONES);
-				
-				while(rs.next()){
-					int region_i = rs.getInt(1); 
-					String region_name = rs.getString(2); 
-					lista_regions.add(new RegionsDTO(region_i, region_name)); 
- 				}
-				
-				
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	 		
-			return lista_regions; 
-
-	 		
 	
-	}
+	public static RegionsDTO recuperarRegions(int region_id) throws SQLException
+	{
 		
+		/* Método para leer una región de la tabla por su id*/ 
+ 		Connection con = null; 
+ 		PreparedStatement pst = null; 
+ 		RegionsDTO region = null; 
+ 		ResultSet rs=null; 
+ 		try { 
+ 			con = Conexion.obtenerConexion(); 
+ 			pst = con.prepareStatement(InstruccionSQL.RECUPERAR_REGIONES); 
+ 			pst.setInt(1, region_id); 
+ 			rs = pst.executeQuery(); 
+ 			 
+ 			while (rs.next()) { 
+ 				region = new RegionsDTO(rs.getInt(1), rs.getString(2)); 
+ 			} 
+ 		} catch (SQLException e) { 
+			e.printStackTrace(); 
+ 			log.error("Error al recuperar id"); 
+ 			throw e; 
+ 		} finally { 
+			//Conexion.liberarRecursos(con, pst, rs);
+ 		}
+	
+ 		return region; 
+	}
+//			Connection conexion = null; 
+//	 		Statement stmt = null; 
+//	 		PreparedStatement pstmt=null;
+//	 		ResultSet rs = null; 
+//	 		RegionsDTO regionsDTO= null;
+//	 		List<RegionsDTO> lista_regions=new ArrayList<RegionsDTO>();
+//	 		
+//			try {
+//				conexion = Conexion.obtenerConexion();
+////				pstmt = conexion.prepareStatement(stmt);
+//				pstmt = conexion.prepareStatement(InstruccionSQL.RECUPERAR_REGIONES);
+//				pstmt.setInt(1,region_id);
+//				
+//				rs=pstmt.executeQuery();
+//				
+//				while(rs.next()){
+//					int region_i = rs.getInt(1); 
+//					String region_name = rs.getString(2); 
+//					lista_regions.add(new RegionsDTO(region_i, region_name)); 
+// 				}
+//				
+//				
+//			} catch (SQLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}finally{
+//				//Conexion.liberarRecursos(conexion, pstmt, rs);
+//			}
+//	 		
+//			return lista_regions; 
+
+	
+	public List<RegionsDAO> insertarRegion(RegionsDTO regionsDTO){
+		
+		//metodo para recuperar region 
+		
+		return null;
+		
+	}
+	
 }
